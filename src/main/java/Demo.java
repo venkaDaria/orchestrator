8c10ffae466fae2fc0058c50ac0dfa265774e498
Daria Pydorenko <dariapydorenko@hrk1-lhp-f03322.green.sophos>
2017-06-08 12:43:28 +0300

    demo edit try-catch

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -11,48 +11,64 @@ import exception.ServiceException;
 import model.Volume;
 
 public class Demo {
-	public static void main(String[] args) throws ContainerException, ServiceException {
-		   ImageReference ref = null;
-		   String[] arr = new String[] {
-			   "docker-registry.cloud.sophos/haproxy:dev",
-			   "docker-registry.cloud.sophos/haproxy@sha256:123abc",
-			   "docker-registry.cloud.sophos/haproxy:dev@sha256:123abc",
-		   };
-		   for (String el : arr) {
-			   ref = new ImageReference(el);
-			   System.out.println(ref);
-		   }
-		   
-		   System.out.println("----");
-		   List<Role> roles = new ArrayList<Role>();
-		   roles.add(new Role("1"));
-		   roles.add(new Role("3"));
-		   Service s = new Service("name", ref, new Volume("x/y/z:var/xxx"), new ArrayList<>(), roles);
-		   roles.clear();
-		   roles.add(new Role("2"));
-		   roles.add(new Role("3"));
-		   Node n = new Node(roles);
-		   Container c = new Container(n);
-		   System.out.println(s.getContainers());
-		   c.start(s);	
-		   System.out.println(s.getRoles());
-		   
-		   System.out.println("----");
-		   System.out.println(s.getContainers());
-		   c.stop(s);
-		   System.out.println(s.getContainers());
-		   c.start(s);
-		   System.out.println(s.getContainers());
-		   System.out.println(s.getNodes());
-		   c.release(s);
-		   System.out.println(s.getContainers());
-		   
-		   System.out.println("----");
-		   n.clearContainers();
-		   Node n2 = new Node(roles);
-		   Container c2 = new Container(n);
-		   n.addContainer(c2);
-		   n.printContainers();
-		   n2.addContainer(c2);
-	   }
+	public static void main(String[] args) {
+		ImageReference ref = null;
+		String[] arr = new String[] { "docker-registry.cloud.sophos/haproxy:dev",
+				"docker-registry.cloud.sophos/haproxy@sha256:123abc",
+				"docker-registry.cloud.sophos/haproxy:dev@sha256:123abc", };
+		for (String el : arr) {
+			ref = new ImageReference(el);
+			System.out.println(ref);
+		}
+
+		System.out.println("----");
+		List<Role> roles = new ArrayList<Role>();
+		roles.add(new Role("1"));
+		roles.add(new Role("3"));
+		Service s = new Service("name", ref, new Volume("x/y/z:var/xxx"), new ArrayList<>(), roles);
+		roles.clear();
+		roles.add(new Role("2"));
+		roles.add(new Role("3"));
+		Node n = new Node(roles);
+		Container c = null;
+		try {
+			c = new Container(n);
+		} catch (ContainerException e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println(s.getContainers());
+		try {
+			c.start(s);
+		} catch (ServiceException e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println(s.getRoles());
+
+		System.out.println("----");
+		System.out.println(s.getContainers());
+		c.stop(s);
+		System.out.println(s.getContainers());
+		try {
+			c.start(s);
+		} catch (ServiceException e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println(s.getContainers());
+		System.out.println(s.getNodes());
+		c.release(s);
+		System.out.println(s.getContainers());
+
+		System.out.println("----");
+		n.clearContainers();
+		Node n2 = new Node(roles);
+		Container c2;
+		try {
+			c2 = new Container(n);
+			n.addContainer(c2);
+			n.printContainers();
+			n2.addContainer(c2);
+		} catch (ContainerException e) {
+			System.out.println(e.getMessage());
+		}	
+	}
 }
