45cc10238027ad4f035cb62979b8ad7443c27fac
Daria Pydorenko <venka.daria@gmail.com>
2017-06-21 13:21:50 +0300

    add javadoc for Translator

--- orchestrator-base/src/main/java/com/globallogic/orchestrator/base/Translator.java
+++ orchestrator-base/src/main/java/com/globallogic/orchestrator/base/Translator.java
@@ -1,5 +1,10 @@
 package com.globallogic.orchestrator.base;
 
+/**
+ * Translates model to dto and backwards
+ * @param <T> a model
+ * @param <R> a dto
+ */
 public interface Translator<T, R> {
 
     R getDto(T model);
