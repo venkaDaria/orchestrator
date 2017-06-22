21554b1658447cd34dc6e983c180a389b3088e24
Daria Pydorenko <venka.daria@gmail.com>
2017-06-22 17:37:13 +0300

    edit gradle dependencies

--- orchestrator-application/src/main/java/com/globallogic/orchestrator/Application.java
+++ orchestrator-application/src/main/java/com/globallogic/orchestrator/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.globallogic.orchestrator")
+@ComponentScan
 public class Application {
 
     public static void main(String[] args) {
