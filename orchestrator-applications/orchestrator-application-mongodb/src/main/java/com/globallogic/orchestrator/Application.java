7a421d243ea605aac55e7d38774fc25b9de17d45
Daria Pydorenko <venka.daria@gmail.com>
2017-06-29 15:53:50 +0300

    add mongo driver

--- orchestrator-applications/orchestrator-application-mongodb/src/main/java/com/globallogic/orchestrator/Application.java
+++ orchestrator-applications/orchestrator-application-mongodb/src/main/java/com/globallogic/orchestrator/Application.java
@@ -2,10 +2,11 @@ package com.globallogic.orchestrator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @ComponentScan("com.globallogic.orchestrator")
 public class Application extends WebMvcConfigurerAdapter {
 
