76240839a6ed3b55a83d082231cac7fc5440b6fe
Daria Pydorenko <venka.daria@gmail.com>
2017-06-30 12:41:13 +0300

    start logging and props

--- orchestrator-applications/orchestrator-application-mongodb/src/main/java/com/globallogic/orchestrator/Application.java
+++ orchestrator-applications/orchestrator-application-mongodb/src/main/java/com/globallogic/orchestrator/Application.java
@@ -4,12 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.apache.log4j.Logger;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @ComponentScan("com.globallogic.orchestrator")
 public class Application {
 
+    private static final Logger LOG = Logger.getLogger(Application.class);
+
     public static void main(String[] args) {
+        LOG.debug("Start app");
         SpringApplication.run(Application.class, args);
     }
 }
+
+//TODO: logging
+//TODO: properties
