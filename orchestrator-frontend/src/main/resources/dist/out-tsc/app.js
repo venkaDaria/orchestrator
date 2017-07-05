7e8e5876c613d426b24f3adcf2dc7de4d29da031
Daria Pydorenko <venka.daria@gmail.com>
2017-07-05 13:10:13 +0300

    add tsconfig.json

--- orchestrator-frontend/src/main/resources/dist/out-tsc/app.js
+++ orchestrator-frontend/src/main/resources/dist/out-tsc/app.js
@@ -1,13 +1,11 @@
-"use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-exports.__esModule = true;
-var core_1 = require("@angular/core");
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+import { Component } from '@angular/core';
+import { bootstrap } from '@angular/platform-browser-dynamic';
 var AppComponent = (function () {
     function AppComponent() {
         this.name = 'World';
@@ -15,6 +13,7 @@ var AppComponent = (function () {
     return AppComponent;
 }());
 AppComponent = __decorate([
-    core_1.Component({ selector: 'greeting', template: 'Hello, {{name}}!' })
+    Component({ selector: 'greeting', template: 'Hello, {{name}}!' })
 ], AppComponent);
-platform_browser_dynamic_1.bootstrap(AppComponent);
+bootstrap(AppComponent);
+//# sourceMappingURL=/Users/dariapydorenko/Documents/workspace/orchestrator/orchestrator-frontend/src/main/resources/static/app.js.map
