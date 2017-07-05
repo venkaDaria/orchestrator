6b7f9044ea7f414636e1040a215a9df9ca657849
Daria Pydorenko <venka.daria@gmail.com>
2017-07-05 11:46:15 +0300

    frontend + thymeleaf

--- orchestrator-controller/src/main/java/com/globallogic/orchestrator/controller/HomeController.java
+++ orchestrator-controller/src/main/java/com/globallogic/orchestrator/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.globallogic.orchestrator.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String index() {
-        return "redirect:/swagger-ui.html";
+    public String index(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
     }
 }
